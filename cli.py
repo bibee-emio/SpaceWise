5a3a450e95a977f993c71433d63ec84d95044383
Bibee <93535892+bibee-emio@users.noreply.github.com>
2023-09-29 16:47:11 +0530

    Update command line UI

--- cli.py
+++ cli.py
@@ -1,3 +1,5 @@
+import argparse
+
 welcome = '''
 ███████╗██████╗  █████╗  ██████╗███████╗██╗    ██╗██╗███████╗███████╗
 ██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝██║    ██║██║██╔════╝██╔════╝
@@ -9,13 +11,43 @@ An ideal educational tool for space enthusiasts and students🚀
 '''
 
 
+class InteractiveCLI:
 
-def print_welcome() -> None:
-    print(welcome)
+    def __init__(self,
+                 categories: list = [
+            'spacex',
+            'artemis',
+            'commercial',
+            'ISS'
+        ] 
+                 ):
+        self.categories = categories
+
+    def print_welcome(self):
+        print(welcome)
 
 
+    def category_table(self) -> int:
+        print('Choose a category;')
+        for i,c in enumerate(self.categories,1):
+            print(f'\t{i}) {c.title()}')
+        
+        while True:
+            category = input('SpaceWise: ')
+            try:
+                category = int(category)
+                if category in range(len(self.categories)+1):
+                    return category
+                print('[!] Please enter a valid category number.')
+            except ValueError:
+                print('[!] Please enter an Integer.')
+
+    def run(self):...
+        
 
 
 
 if __name__ == '__main__':
-    print_welcome()
+    cli1 = InteractiveCLI()
+    print(welcome)
+    cli1.category_table()
--- main.py
+++ main.py
@@ -1,45 +1,24 @@
-import os
-from datetime import datetime
-from PIL import Image
-from docx.image.exceptions import UnrecognizedImageError
 
-from helper import SpecialFuntions, DocumentWriter
+
+from cli import InteractiveCLI
 from crawler import NasaSpaceFlight
 
 
+if __name__=='__main__':
+    interactive_cli = InteractiveCLI()
+    interactive_cli.print_welcome()
 
-funcs = SpecialFuntions()
-nasa = NasaSpaceFlight('ARTEMIS'.lower())
-#nasa = SpaceX()
+    category =  interactive_cli.category_table()
+    '''
+    Add a loading with threading for connecting...,downlaoding... etc.
+    '''
+    print('Connecting...')
+    nasa_space_flight = NasaSpaceFlight(interactive_cli.categories[category-1].lower())
 
-
-
-print('Scraping data...')
-header = nasa.get_headline()
-print(header)
-exit()
-paragrph = nasa.get_paragraph()
-
-print('Downloading images...')
-#urls = nasa.get_image_urls()
-#images  = funcs.download_images(urls)
-doc_name = f'downloads/SpaceX/SpaceX-{datetime.now().date()}.docx'
-
-print('Writing to a document...')
-doc = DocumentWriter(doc_name)
-
-doc.write_header(header+'\n')
-doc.add_image('src/pic-1.jpg') # Adding the lead image
-doc.write_paragraph(paragrph)
-
-# Adding other all remaining images to the end of the document
-'''for i in range(2,images):
-    img = f'src/pic-{i}.jpg'
-    try:
-        doc.add_image(img)
-    except UnrecognizedImageError:
-        Image.open(img).save(img)
-        doc.add_image(img)
-'''
-doc.save_document()
-print(f'Document Saved to {os.path.abspath(doc_name)}')
+    header = nasa_space_flight.get_headline()
+    
+    print(f'[Latest Article]: {header}')
+    if (input('Do you need to download it [y/n]?: ') + ' ')[0].lower() != 'y':
+        exit(0)
+    print('works')
+        
